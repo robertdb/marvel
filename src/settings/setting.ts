421c2bde891ad307c9ea41d5f484660bdb5973aa
Robert Diaz Bejar <robertdibejar@gmail.com>
2019-04-16 18:33:22 -0300

    update to https

--- src/settings/setting.ts
+++ src/settings/setting.ts
@@ -1,4 +1,4 @@
-export const API = 'http://gateway.marvel.com/v1/public/';
+export const API = 'https://gateway.marvel.com/v1/public/';
 export const ts = '61781402';
 export const apikey = 'e76320ce4b68d79d3a6d8854016aecc4';
 export const hash = '8166a944e9a09d37c9ca77e1b67479ea';
