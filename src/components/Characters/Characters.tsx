6a0c982554ec63f0690c3fd34746e441d33f38f8
Robert Diaz Bejar <robertdibejar@gmail.com>
2019-04-16 19:03:10 -0300

    element fixed with any type

--- src/components/Characters/Characters.tsx
+++ src/components/Characters/Characters.tsx
@@ -1,29 +1,19 @@
 import * as React from 'react';
 import { Props, ItemProps } from './';
-
+import ItemCharacter from './ItemCharacter';
 import {
     ContainerCharacters,
-    ContainerCharacter,
-    TitleCharacter,
-    ImageCharacter
+ 
 } from './style';
 
 const Characters = ({ characters }: Props) => {
     return (
         <ContainerCharacters>
             {characters.map((character: ItemProps) => {
-                const { id, name, thumbnail } = character;
+                const { id } = character;
                 return (
                     <React.Fragment key={id}>
-                        <ContainerCharacter>
-                            <TitleCharacter>
-                                {name}
-                            </TitleCharacter>
-                            <ImageCharacter
-                                src={`${thumbnail.path}.${thumbnail.extension}`}
-                                alt="character"
-                            />
-                        </ContainerCharacter>
+                        <ItemCharacter item={character}/>
                     </React.Fragment>
                 )
 
--- src/components/Characters/ItemCharacter.tsx
+++ src/components/Characters/ItemCharacter.tsx
@@ -1,18 +1,17 @@
 import * as React from 'react';
-import { ItemProps } from './';
+//import { ItemProps } from './';
 import {
     ContainerCharacter,
     TitleCharacter,
     ImageCharacter
 } from './style';
 
-const ItemCharacter = (props: ItemProps) => {
-    const { name, thumbnail } = props;
-    console.log('character', typeof props);
+const ItemCharacter = ({ item }: any) => {
+    const { name, description, thumbnail } = item;
     return (
         <ContainerCharacter>
             <TitleCharacter>
-                {name}
+                {name}<br />{description}
             </TitleCharacter>
             <ImageCharacter
                 src={`${thumbnail.path}.${thumbnail.extension}`}
